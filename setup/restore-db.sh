e5bfe3c752b9235cc0c586b268546afd9530ac9c
Adam Heath <doogie@brainfood.com>
2020-06-05 16:25:04 -0500

    Read secrets into environment variables.

--- setup/restore-db.sh
+++ setup/restore-db.sh
@@ -9,6 +9,11 @@ done
 
 ls /run/secrets
 
+export AWS_ACCESS_KEY_ID="$(cat /run/secrets/aws-access-key-id)"
+export AWS_SECRET_ACCESS_KEY="$(cat /run/secrets/aws-secret-access-key)"
+
+export RESTIC_PASSWORD="$(< /run/secrets/restic-password)"
+
 restic dump \
   -p /run/secrets/restic-password \
   -H $RESTIC_HOST
