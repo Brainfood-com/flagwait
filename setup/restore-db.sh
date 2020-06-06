aa66bcd80c6f99bb8e66b9ccc3d4426f0089e17b
Ean Schuessler <ean@brainfood.com>
2020-06-06 00:33:12 -0500

    Successfuly restores database from restic

--- setup/restore-db.sh
+++ setup/restore-db.sh
@@ -7,16 +7,18 @@ while ! mysqladmin ping -h $MYSQL_HOST --silent; do
   sleep 1
 done
 
-ls /run/secrets
+echo "Database is up. Restoring archive."
 
 export AWS_ACCESS_KEY_ID="$(cat /run/secrets/aws-access-key-id)"
 export AWS_SECRET_ACCESS_KEY="$(cat /run/secrets/aws-secret-access-key)"
+export RESTIC_PASSWORD="$(cat /run/secrets/restic-password)"
+export MYSQL_PASSWORD="$(cat /run/secrets/mysql-password)"
+export BACKUP=$(restic -p /run/secrets/restic-password -H $RESTIC_HOST ls latest /backups | tail -n 1)
 
-export RESTIC_PASSWORD="$(< /run/secrets/restic-password)"
-
-restic dump \
+restic \
   -p /run/secrets/restic-password \
-  -H $RESTIC_HOST
+  -H $RESTIC_HOST \
+  dump latest $BACKUP | zcat | mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE
 
 echo "Restore complete"
 touch /var/run/bfflags/loaded
